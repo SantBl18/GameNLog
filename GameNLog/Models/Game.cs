30586a1f642223a95fdd99f7a4bcda701a66d2c6
SantBl18 <sballenl@unal.edu.co>
2026-03-02 22:53:50 -0500

    Add project files.

---
+++ GameNLog/Models/Game.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GameNLog.Models
+{
+    public class Game
+    {
+        [Key]
+        public int GameID { get; set; }
+        [Required]
+        public string? Name { get; set; }
+        [Required]
+        public string? slug { get; set; }
+        [Required]
+        public string? summary { get; set; }
+        [ForeignKey("CoverID")]
+        public int CoverID { get; set; }
+        [DataType(DataType.DateTime)]
+        public DateTime FirstReleaseDate { get; set; }
+        [DataType(DataType.DateTime)]
+        public DateTime UpdatedAt { get; set; }
+
+    }
+}
